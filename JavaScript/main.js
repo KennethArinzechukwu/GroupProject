97488c78a234a96bccfcd8d541617f4c8637d091
Nzelu-Victor <ebubevictor44@gmail.com>
2022-07-11 09:54:37 +0100

    nigga

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,8 +1,113 @@
-$(window).scroll(function () {
-    if ($(window).scrollTop() >= 50) {
-        $('#navBg').addClass('fixed-top bg-nav');
+
+
+(function() {
+    "use strict";
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() >= 50) {
+            $('#navBg').addClass('fixed-top bg-nav');
+        }
+        else {
+            $('#navBg').removeClass('fixed-top bg-nav');
+        }
+    });
+    
+
+    
+    //  Easy selector helper function
+     
+     const select = (el, all = false) => {
+        el = el.trim()
+        if (all) {
+            return [...document.querySelectorAll(el)]
+        } else {
+            return document.querySelector(el)
+        }
     }
-    else {
-        $('#navBg').removeClass('fixed-top bg-nav');
+
+    
+    //   Easy event listener function
+     
+    const on = (type, el, listener, all = false) => {
+        let selectEl = select(el, all)
+        if (selectEl) {
+            if (all) {
+                selectEl.forEach(e => e.addEventListener(type, listener))
+            } else {
+                selectEl.addEventListener(type, listener)
+            }
+        }
     }
-});
+
+    
+    //   Testimonials slider
+     
+    new Swiper('.testimonials-slider', {
+        speed: 600,
+        loop: true,
+        autoplay: {
+            delay: 5000,
+            disableOnInteraction: false
+        },
+        slidesPerView: 'auto',
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+            clickable: true
+        }
+    });
+
+    
+    //  Porfolio isotope and filter
+     
+    window.addEventListener('load', () => {
+        let portfolioContainer = select('.portfolio-container');
+        if (portfolioContainer) {
+            let portfolioIsotope = new Isotope(portfolioContainer, {
+                itemSelector: '.portfolio-item',
+                layoutMode: 'fitRows'
+            });
+
+            let portfolioFilters = select('#portfolio-flters li', true);
+
+            on('click', '#portfolio-flters li', function(e) {
+                e.preventDefault();
+                portfolioFilters.forEach(function(el) {
+                    el.classList.remove('filter-active');
+                });
+                this.classList.add('filter-active');
+
+                portfolioIsotope.arrange({
+                    filter: this.getAttribute('data-filter')
+                });
+
+            }, true);
+        }
+
+    });
+
+    
+    //  Initiate portfolio lightbox 
+     
+    const portfolioLightbox = GLightbox({
+        selector: '.portfolio-lightbox'
+    });
+
+    
+    //   Portfolio details slider
+     
+    new Swiper('.portfolio-details-slider', {
+        speed: 400,
+        loop: true,
+        autoplay: {
+            delay: 5000,
+            disableOnInteraction: false
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+            clickable: true
+        }
+    });
+
+})()
